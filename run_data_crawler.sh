6da94cd8034aaa93d1f523bf3e18515a269441bc
Vincent Zhu <111105725+vincent05r@users.noreply.github.com>
2025-12-09 16:06:23 +1100

    code rf
    remove the data crawler from this repo, swap to another repo

--- run_data_crawler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Correctly initialize conda for non-interactive shell
-source ~/miniconda3/etc/profile.d/conda.sh
-
-conda activate FTS_download
-
-python /home/vincent-1080/repo/Finance_data/data_crawl.py
