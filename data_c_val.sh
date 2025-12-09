d6a9488c68ac56086e61075c350326c964c3236b
Vincent Zhu <111105725+vincent05r@users.noreply.github.com>
2025-12-09 15:10:40 +1100

    s

--- data_c_val.sh
+++ data_c_val.sh
@@ -1,3 +1,3 @@
 python data_combine_val.py \
-  --output-dir /path/to/test_merge \
+  --output-dir /home/vincent-1080/repo/Finance_data/merge_m_250715_250813 \
   --fail-on any   # options: none, gaps_mismatch, schema, any
