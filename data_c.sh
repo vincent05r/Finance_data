050043ea1253809d27bb8df08ce4c8338988bd2b
Vincent Zhu <111105725+vincent05r@users.noreply.github.com>
2025-12-12 14:57:32 +1100

    sync

--- data_c.sh
+++ data_c.sh
@@ -1,5 +1,5 @@
 python data_combine.py \
-  --input-root /home/vincent-1080/repo/Finance_data/FTS_data_snapshots \
-  --output-dir ./merge_m_250715_250813 \
+  --input-root /home/vincent-1080/repo/Finance_data/merge_m_251013_251201 \
+  --output-dir ./merge_m_251013_251201 \
   --tz-strategy to_utc \
   --assume-naive-tz UTC
--- data_c_val.sh
+++ data_c_val.sh
@@ -1,3 +1,3 @@
 python data_combine_val.py \
-  --output-dir /home/vincent-1080/repo/Finance_data/merge_m_250715_250813 \
+  --output-dir /home/vincent-1080/repo/Finance_data/merge_m_251013_251201 \
   --fail-on any   # options: none, gaps_mismatch, schema, any
